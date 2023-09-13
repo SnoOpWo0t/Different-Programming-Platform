9eab2e85ce77a726aeceb86276ff62bd0ad16570
Afrin Sultana Akhi <afrinsultanaakhi138@gmail.com>
2023-09-13 20:52:48 +0600

    k

---
+++ HAckerRank/Conditional Statements.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main ()
+{
+    int n;
+    cin>>n ;
+    if(n>=1 && n<=9)
+    {
+        if(n==1)
+        {cout<<"one"<<endl;}
+         if(n==2)
+        {cout<<"two"<<endl;}
+         if(n==3)
+        {cout<<"three"<<endl;}
+         if(n==4)
+        {cout<<"four"<<endl;}
+         if(n==5)
+        {cout<<"five"<<endl;}
+         if(n==6)
+        {cout<<"six"<<endl;}
+         if(n==7)
+        {cout<<"seven"<<endl;}
+         if(n==8)
+        {cout<<"eight"<<endl;}
+         if(n==9)
+        {cout<<"nine"<<endl;}
+    
+    
+    }
+    else
+    {
+        cout<<"Greater than 9"<<endl;
+    }
+
+}
