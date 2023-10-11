5e5f63dc6e59213163e240aaffd92b6beb6b3840
Afrin Sultana Akhi <afrinsultanaakhi138@gmail.com>
2023-10-11 10:39:06 +0600

    k

---
+++ Kattis/Framtíðar FIFA.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main ()
+{
+   // int i;
+   // for(i=2 ; i>=0;i--)
+  //  {
+        int a,b ;
+            cin>>a>>b;
+            int d = a/b;
+           // int sum = 2022+d;
+            cout<<d+2022<<endl;
+
+  //  }
+}
