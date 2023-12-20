ffab4e31776d0d61f33cc171556d55e2e0a796f9
Afrin Sultana Akhi <afrinsultanaakhi138@gmail.com>
2023-12-20 09:53:01 +0600

    Create millifaersla.cpp

---
+++ Kattis/millifaersla.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main ()
+{
+    int a,b,c;
+    cin >> a >> b >> c;
+  //  int max = max(max(a,b),c);
+    int minv = min(min(a,b),c);
+   if(minv ==a)
+   cout<<"Monnei"<<endl;
+   else if(minv ==b)
+   cout<<"Fjee"<<endl;
+    else
+    cout<<"Dolladollabilljoll"<<endl;
+
+}
