c8e428ff5e1eff1dc06c0a4f9baf8668b4f047a8
Afrin Sultana Akhi <afrinsultanaakhi138@gmail.com>
2024-01-05 21:58:06 +0600

    k

---
+++ Kattis/nsum.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main ()
+{
+    int t ,sum = 0;
+    cin >>t ;
+    for(int i = 0 ; i<t ; i++)
+    {
+        int n[t] ;
+        cin >>n[i] ;
+        sum += n[i] ;
+
+    }cout <<sum ;
+}
