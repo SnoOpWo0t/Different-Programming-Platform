504c3895a4d5a7c1146c7a4194a8b63ee2f0308b
Afrin Sultana Akhi <afrinsultanaakhi138@gmail.com>
2024-02-21 19:35:32 +0600

    k

---
+++ CodeForces/1925OddoneOut.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+      int t;
+   cin >> t;
+
+    while (t--) {
+        int a, b, c;
+      cin >> a >> b >> c;
+
+        // Check which digit occurs exactly once
+        if (a == b) {
+           cout << c << endl;
+        } else if (a == c) {
+            cout << b << endl;
+        } else {
+            cout << a << endl;
+        }
+    }
+
+}
