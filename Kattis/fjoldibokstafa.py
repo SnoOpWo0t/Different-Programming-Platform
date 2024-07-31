c29ac48f5c6ec93cd0195f9baf05005e332eb365
Afrin Sultana Akhi <afrinsultanaakhi138@gmail.com>
2024-07-31 17:43:02 +0600

    k

---
+++ Kattis/fjoldibokstafa.py
@@ -0,0 +1,6 @@
+a = input()
+count = 0
+for i in a:
+    if i.isalpha():
+        count+=1
+print(count)
--- Kattis/grassseed.py
+++ Kattis/grassseed.py
@@ -1,9 +1,9 @@
 seedcost = float(input())
-lawns = float(input())
+lawns = int(input())
 cost = 0
 
 for lawn in range(lawns):
     w,l = list(map(float,input().split()))
     area = w*l
     cost += area*seedcost
-print(cost)
+print(float(cost))
