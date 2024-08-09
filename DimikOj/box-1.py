fa33a5859e21cd8b7129880db35f5209a5f1b38d
Afrin Sultana Akhi <afrinsultanaakhi138@gmail.com>
2024-08-09 11:02:43 +0600

    k

--- DimikOj/box-1.py
+++ DimikOj/box-1.py
@@ -1,8 +1,20 @@
-test = int(input())
-for test in range(test):
+t = int(input())
+
+for case in range(t):
     n = int(input())
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        print('*',end='')
-    print()
     
+    for i in range(n):
+        print('*' * n)
+    
+    # Print a blank line between squares, but not after the last one
+    if case < t - 1:
+        print()
+
+# test = int(input())
+#     for test in range(test):
+#         n = int(input())
+#         for i in range(1,n+1):
+#             for j in range(1,n+1):
+#                 print('*',end='')
+#             print() ---
+     
