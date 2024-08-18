f8cbd29bea101e026b8a81e6f92d5389690bac99
Afrin Sultana Akhi <afrinsultanaakhi138@gmail.com>
2024-08-18 22:16:42 +0600

    rf

---
+++ HAckerRank/solve-me-first.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+  int a,b;
+  cin>>a>>b;
+  int sum = a+b;
+  cout<<sum<<endl;   
+    return 0;
+}
