745d0d70d22e1804a6d21528ace9bed60651bad6
Afrin Sultana Akhi <afrinsultanaakhi138@gmail.com>
2025-12-12 10:20:59 +0600

    k

---
+++ Kattis/skeytasaman.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+     string a,b;
+     cin>>a;
+     cin>>b;
+     cout<<a+b<<endl;
+}
---
+++ Testpractice after-longtime/hel.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main()
+{ int x,y,z=0;
+     cin>>x>>y;
+
+ z =(x+10)/(3*y); 
+ cout<<z;
+}
+
